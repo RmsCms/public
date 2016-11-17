d1cd5af069fb21513ccdeb761d4c97042e1d5b24
Sharif Ahrari <msharif.ahrari@gmail.com>
2016-11-17 14:01:09 +0330

    add unregister to module position

--- admin/js/position.js
+++ admin/js/position.js
@@ -20,4 +20,27 @@ $(document).ready(function(){
          })
       }
    });
+    // unregister module
+    $(document).on('click','.unregister',function(e){
+        e.preventDefault();
+        var id = $(this).closest('tr').attr('id');
+        var element = $(this);
+        $.ajax({
+            type:'POST',
+            url:ajax_position_unregister,
+            dataType:'json',
+            data:{'_token':_token,'data':id},
+            success:function(dataJson){
+                if(dataJson.status=='success')
+                {
+                    $.growl.notice({title:'',message:dataJson.msg});
+                    element.closest('tr').remove();
+                }
+                else
+                {
+                    $.growl.error({title:'',message:dataJson.msg});
+                }
+            }
+        });
+    });
 });
