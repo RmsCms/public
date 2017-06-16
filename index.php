ea16c42d907fcb5096ea2f5a32245dbc8835e188
msharif ahrari <msharif.ahrari@gmail.com>
2017-06-16 22:17:22 +0430

    fix some bugs

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../../ticket/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,8 +33,12 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../../ticket/bootstrap/app.php';
 
+// set the public path to this directory
+$app->bind('path.public', function() {
+    return __DIR__;
+});
 /*
 |--------------------------------------------------------------------------
 | Run The Application
